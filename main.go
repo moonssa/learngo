a514a0249837a53d03fab01d229617da7244a09d
moonk <mkjoehsb@gmail.com>
2022-07-25 13:41:13 +0900

    pointer struct slice

--- main.go
+++ main.go
@@ -1,78 +1,43 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func lenAndUpper(name string)(int, string){
-	return len(name), strings.ToUpper(name)
+type  person struct {
+	name string
+	age int
+	favFood []string
 }
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
-}
 
-func lenAndUpper2(name string)(length int, uppercase string) {
-	defer fmt.Println("I'm done")
-	fmt.Println("defer test")
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return  // naked return
-}
+func main() {
+	favFood :=[]string{"kimchi", "ramen"}
+	moon := person {"moon", 25, favFood} // 필드가 의미하는 바가 명확하지 않다. 아래와 같이 사용하는게 좋다
+	heon := person {name:"heon", age:26, favFood:favFood}
 
-func superAdd_test(numbers ...int) int {
-	for index, number := range numbers {
-		fmt.Println(index, number)
+
+	fmt.Println(moon)
+	fmt.Println(heon)
+
+	a :=2
+	b := &a
+	*b = 20
+	fmt.Println(&a, b)
+	fmt.Println(*b, a)
+
+	names := [5] string {"bb", "dd", "aa", "hh", "jj"}
+	fmt.Println(names)
+
+	nickname :=[] string {"haha"}
+	nickname = append(nickname, "hihi")
+	fmt.Println(nickname)
+
+	nico :=map[string]string {"name": "nico", "age":"15"}
+	fmt.Println(nico)
+
+	for key, value := range nico {
+		fmt.Println(key, value)
 	}
-
-	fmt.Println("traditional")
-	for i:=0; i < len(numbers); i ++ {
-		fmt.Println(numbers[i])
+	for _, value := range nico {
+		fmt.Println( value)
 	}
-	return 1
-}
-func superAdd(numbers ...int) int {
-	total := 0
-	for _, number := range numbers {
-		fmt.Println(number)
-		total += number
-	}
-	return total
-}
-func canIDrink(age int) bool {
-	if koreanAge := age+2; koreanAge < 18 {
-		return false
-	} 
-	return true
-	
-}
-
-func canIDrinkTest(age int) bool {
-	switch koreanAge := age+2 ; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
-func main(){
-	fmt.Println(canIDrinkTest(8))
-	fmt.Println(canIDrinkTest(16))
-
-	// fmt.Println(canIDrink(15))
-	// fmt.Println(canIDrink(16))
-
-	// result := superAdd(1,3,5,7,9,11)
-	// fmt.Println(result)
-	// totalLen, Upp := lenAndUpper("moon")
-	// fmt.Println(totalLen, Upp)
-	// tLen , _ := lenAndUpper("moon") // return 값 생략 가능 _ 이용
-	// fmt.Println(tLen)
-	// repeatMe("moon", "happy", "thanks")
-	
-	// totalLen2, Upp2:= lenAndUpper2("honey")
-	// fmt.Println(totalLen2, Upp2)
-
 }
