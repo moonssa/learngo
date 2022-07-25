c2ca2adeee0e88f83d848858175e1998aace0770
moonk <mkjoehsb@gmail.com>
2022-07-25 10:54:31 +0900

    for loop

--- main.go
+++ main.go
@@ -21,14 +21,35 @@ func lenAndUpper2(name string)(length int, uppercase string) {
 	return  // naked return
 }
 
+func superAdd_test(numbers ...int) int {
+	for index, number := range numbers {
+		fmt.Println(index, number)
+	}
+
+	fmt.Println("traditional")
+	for i:=0; i < len(numbers); i ++ {
+		fmt.Println(numbers[i])
+	}
+	return 1
+}
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		fmt.Println(number)
+		total += number
+	}
+	return total
+}
 func main(){
-	totalLen, Upp := lenAndUpper("moon")
-	fmt.Println(totalLen, Upp)
-	tLen , _ := lenAndUpper("moon") // return 값 생략 가능 _ 이용
-	fmt.Println(tLen)
-	repeatMe("moon", "happy", "thanks")
+	result := superAdd(1,3,5,7,9,11)
+	fmt.Println(result)
+	// totalLen, Upp := lenAndUpper("moon")
+	// fmt.Println(totalLen, Upp)
+	// tLen , _ := lenAndUpper("moon") // return 값 생략 가능 _ 이용
+	// fmt.Println(tLen)
+	// repeatMe("moon", "happy", "thanks")
 	
-	totalLen2, Upp2:= lenAndUpper2("honey")
-	fmt.Println(totalLen2, Upp2)
+	// totalLen2, Upp2:= lenAndUpper2("honey")
+	// fmt.Println(totalLen2, Upp2)
 
 }
